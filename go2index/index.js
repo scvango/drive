2fe94b2992ac5728657cd8f56f406f6580b9fb36
Scvango <scvango@gmail.com>
2021-01-02 13:12:44 +0530

    :package: 2.1.1

--- go2index/index.js
+++ go2index/index.js
@@ -67,7 +67,7 @@ var authConfig = {
 var themeOptions = {
   cdn: "https://cdn.jsdelivr.net/gh/scvango/drive",
   // 主题版本号
-  version: "2.1.0",
+  version: "2.1.1",
   //可选默认系统语言:en/zh-chs/zh-cht
   languages: "en",
   render: {
